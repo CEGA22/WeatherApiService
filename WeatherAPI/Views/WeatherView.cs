b589ca2d52bee1891c16d78e2cec1792ee348f9a
Ceejay Gabrang <71625955+CEGA22@users.noreply.github.com>
2022-06-24 14:02:39 +0800

    Added error message in WeatherView

--- WeatherAPI/Views/WeatherView.cs
+++ WeatherAPI/Views/WeatherView.cs
@@ -7,18 +7,41 @@ using WeatherAPI;
 
 WeatherService weatherService = new WeatherService();
 var country = await weatherService.GetCountryWeather();
-var countryXml = await weatherService.GetCountryWeatherXml();
 Console.WriteLine("JSON response");
-Console.WriteLine($"Country:{country.name}");
-Console.WriteLine($"Weather:{country.weather[0].description}");
-Console.WriteLine($"Temperature:{country.main.temp}");
-Console.WriteLine($"Humidity:{country.main.humidity}");
+
+if (country == null)
+{
+    Console.WriteLine("There was error processing request");
+}
+else
+{
+    Console.WriteLine($"Country:{country.name}");
+    Console.WriteLine($"Weather:{country.weather[0].description}");
+    Console.WriteLine($"Temperature:{country.main.temp}");
+    Console.WriteLine($"Humidity:{country.main.humidity}");
+}
+
 Console.WriteLine("===================================");
+
+var countryXml = await weatherService.GetCountryWeatherXml();
 Console.WriteLine("XML response");
-Console.WriteLine($"Country:{countryXml.City.Name}");
-Console.WriteLine($"Weather:{countryXml.Clouds.Name}");
-Console.WriteLine($"Temperature:{countryXml.Temperature.Value}");
-Console.WriteLine($"Humidity:{countryXml.Humidity.Value}");
+
+if (countryXml == null)
+{
+    Console.WriteLine("There was error processing request");
+}
+
+else
+{
+    
+    Console.WriteLine($"Country:{countryXml.City.Name}");
+    Console.WriteLine($"Weather:{countryXml.Clouds.Name}");
+    Console.WriteLine($"Temperature:{countryXml.Temperature.Value}");
+    Console.WriteLine($"Humidity:{countryXml.Humidity.Value}");   
+}
 Console.ReadLine();
+
+
+
 
 
