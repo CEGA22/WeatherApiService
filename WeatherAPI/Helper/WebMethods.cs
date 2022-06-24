bc09cfbef784f8f72eaaf022ded41f778c50a835
Ceejay Gabrang <71625955+CEGA22@users.noreply.github.com>
2022-06-24 09:44:09 +0800

    Modify WebMethods class

--- WeatherAPI/Helper/WebMethods.cs
+++ WeatherAPI/Helper/WebMethods.cs
@@ -10,66 +10,23 @@ namespace WeatherAPI.Helper
 {
     public class WebMethods
     {
+        static HttpClient _httpClient;
+        
         public static async Task<string> MakeGetRequest(string url)
         {
-
-            var strReturn = string.Empty;
-
             try
             {
-                var request = WebRequest.Create(url);
-                HttpWebResponse response = await request.GetResponseAsync() as HttpWebResponse;
-
-                if (response.StatusCode == HttpStatusCode.OK)
+                _httpClient = new HttpClient
                 {
-                    using (StreamReader reader = new StreamReader(response.GetResponseStream()))
-                    {
-                        strReturn = reader.ReadToEnd();
-
-                        if (string.IsNullOrWhiteSpace(strReturn))
-                        {
-                            strReturn = response.StatusCode + "Response contained an empty body ...";
-                        }
-
-                        return strReturn;
-                    }
-                }
-
-                else
-                {
-                    return "error";
-                }
+                    BaseAddress = new Uri(url)
+                };
+               var results = await _httpClient.GetStringAsync(url);
+               return results; 
             }
-
-            catch (Exception e)
+            catch
             {
                 return "error";
             }
-        }
-
-        public static async Task<string> MakePostRequest(Uri uri, object item)
-        {
-            try
-            {
-                var httpClient = new HttpClient();
-                var json = JsonConvert.SerializeObject(item);
-                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
-                HttpResponseMessage response = await httpClient.PostAsync(uri, content);
-
-                if (response.IsSuccessStatusCode)
-                {
-                    return response.Content.ReadAsStringAsync().Result;
-                }               
-            }
-
-            catch (System.Exception ex)
-            {
-
-                var x = ex.Message;
-                return "error";
-            }
-
-            return string.Empty;
         }
     }
 }
